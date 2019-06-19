0285341beff7dad563135932b599ec3efb431cac
Asad Ali Akram <asad.ali@devtrongenesis.com>
2019-06-19 17:44:17 +0500

    add script and css for video

--- sites/all/themes/health/js/scripts.js
+++ sites/all/themes/health/js/scripts.js
@@ -1301,28 +1301,39 @@ $(window).load(function(){
 });
 
 
-        function youTubes_makeDynamic() {
-            var $ytIframes = $('iframe[src*="youtube.com"]');
-            $ytIframes.each(function (i,e) {
-                var $ytFrame = $(e);
-                var ytKey; var tmp = $ytFrame.attr('src').split(/\//); tmp = tmp[tmp.length - 1]; tmp = tmp.split('?'); ytKey = tmp[0];
-                var $ytLoader = $('<div class="ytLoader">');
-                $ytLoader.append($('<img class="cover" src="https://i.ytimg.com/vi/'+ytKey+'/hqdefault.jpg">'));
-                $ytLoader.append($('<img class="playBtn" src="play_button.png">'));
-                $ytLoader.data('$ytFrame',$ytFrame);
-                $ytFrame.replaceWith($ytLoader);
-                $ytLoader.click(function () {
-                    var $ytFrame = $ytLoader.data('$ytFrame');
-                    $ytFrame.attr('src',$ytFrame.attr('src')+'?autoplay=1');
-                    $ytLoader.replaceWith($ytFrame);
-                });
-            });
-        };
+    /* Light YouTube Embeds by @labnol */
+    /* Web: http://labnol.org/?p=27941 */
+
+    document.addEventListener("DOMContentLoaded",
+        function() {
+            var div, n,
+                v = document.getElementsByClassName("youtube-player");
+            for (n = 0; n < v.length; n++) {
+                div = document.createElement("div");
+                div.setAttribute("data-id", v[n].dataset.id);
+                div.innerHTML = labnolThumb(v[n].dataset.id);
+                div.onclick = labnolIframe;
+                v[n].appendChild(div);
+            }
+        });
+
+    function labnolThumb(id) {
+        var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+            play = '<div class="play"></div>';
+        return thumb.replace("ID", id) + play;
+    }
+
+    function labnolIframe() {
+        var iframe = document.createElement("iframe");
+        var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
 
 
    $(document).ready(function () {
-
-    youTubes_makeDynamic();
 
     $('span.dropdown-toggle').on('click', function(e) {
       e.stopPropagation();
